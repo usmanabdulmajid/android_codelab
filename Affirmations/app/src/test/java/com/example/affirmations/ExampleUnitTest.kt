7d11a2f66c82a869e715bdf66b3c00aa0138f2ff
Abdulmajid Usman <abstractmajid@gmail.com>
2023-06-05 22:01:06 +0100

    affirmations: add unit test

--- Affirmations/app/src/test/java/com/example/affirmations/ExampleUnitTest.kt
+++ Affirmations/app/src/test/java/com/example/affirmations/ExampleUnitTest.kt
@@ -1,8 +1,11 @@
 package com.example.affirmations
 
+import com.example.affirmations.adapter.ItemAdapter
+import com.example.affirmations.models.Affirmation
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.mockito.Mockito.mock
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,6 +13,18 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    private val context = mock(android.content.Context::class.java)
+
+    @Test
+    fun adapter_size(){
+        val data = listOf(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2)
+        )
+        val adapter = ItemAdapter(context, data)
+        assertEquals("ItemAdapter is not the correct size", data.size, adapter.itemCount)
+    }
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
