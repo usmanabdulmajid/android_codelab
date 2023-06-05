d037e024f1a8f48095c8c71254d937d5f6ba831d
Abdulmajid Usman <abstractmajid@gmail.com>
2023-06-05 19:23:17 +0100

    affirmation

---
+++ Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.affirmations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.adapter.ItemAdapter
+import com.example.affirmations.models.Affirmation
+import com.example.affirmations.data.Datasource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val datasource = Datasource()
+        val affirmations = datasource.loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, affirmations)
+        recyclerView.setHasFixedSize(true)
+
+
+
+    }
+}
