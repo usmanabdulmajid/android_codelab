7700aa5895d78d1e22f8264b094b62a21511710c
Abdulmajid Usman <abstractmajid@gmail.com>
2023-06-05 21:49:29 +0100

    affirmations: add ui test

--- Affirmations/app/src/androidTest/java/com/example/affirmations/ExampleInstrumentedTest.kt
+++ Affirmations/app/src/androidTest/java/com/example/affirmations/ExampleInstrumentedTest.kt
@@ -1,5 +1,11 @@
 package com.example.affirmations
 
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +13,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,6 +22,21 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun scroll_to_item(){
+        onView(withId(R.id.recycler_view)).perform(
+            RecyclerViewActions
+                .scrollToPosition<RecyclerView.ViewHolder>(9)
+        )
+
+        onView(withText(R.string.affirmation10))
+            .check(matches(isDisplayed())
+            )
+    }
     @Test
     fun useAppContext() {
         // Context of the app under test.
