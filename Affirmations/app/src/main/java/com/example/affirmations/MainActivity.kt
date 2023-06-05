901a0b2d92ba2230cec512bf5fba64ef834edc20
Abdulmajid Usman <abstractmajid@gmail.com>
2023-06-05 19:49:53 +0100

    affirmations: change linear layout to grid layout for recycler view

--- Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
+++ Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations.adapter.ItemAdapter
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity() {
         val datasource = Datasource()
         val affirmations = datasource.loadAffirmations()
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
         recyclerView.adapter = ItemAdapter(this, affirmations)
         recyclerView.setHasFixedSize(true)
 
